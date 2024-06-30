c74796d07d7fc4cfa508aac89dda74c08a322968
pollovolante <edoardovolantebusiness@gmail.com>
2024-06-30 12:45:56 +0200

    better lighmode

--- src/components/LightModeToggle.tsx
+++ src/components/LightModeToggle.tsx
@@ -1,21 +1,38 @@
 "use client"
 
-import { useEffect, useState } from "react";
+import { useEffect, useState,createElement } from "react";
 import { useMediaQuery } from "react-responsive";
 
-
+//component for light mode toggle
 export default function LightModeToggle() {
-
+  //setting a react hook for darkmode 
   const [isDark, setIsDark] = useState(true);
+  //local function that handles toggle
+  const toggleTheme = (toSetLight: boolean) => {
+    //saving on theme local storage preference
+    localStorage.setItem("theme", toSetLight ? "light" : "dark");
+    //changing the hook state
+    setIsDark(toSetLight);
+  };
 
+  //execute this code on client rendering
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (!theme) {
+      localStorage.setItem("theme", "dark");
+    }
+    toggleTheme(localStorage.getItem("theme") !== "dark")
+  }, []); 
+
+  //see if system preference is dark
   useMediaQuery(
     {
       query: "(prefers-color-scheme: dark)",
     },
     undefined,
-    (isSystemDark) => setIsDark(isSystemDark)
+    (isSystemDark) => toggleTheme(isSystemDark)
   );
-
+  //add/remnove class dark to body in order to change theme ( whatching changes on css)
   useEffect(() => {
     if (isDark) {
       document.body.classList.add('dark');
@@ -23,10 +40,10 @@ export default function LightModeToggle() {
       document.body.classList.remove('dark');
     }
   }, [isDark]);
-
+  //returning the react component
   return (
     <div className="wrapper">
-      <input type="checkbox" id="hide-checkbox" onClick={()=>setIsDark(!isDark)} defaultChecked={!isDark}></input>
+      <input type="checkbox" id="hide-checkbox" onClick={() => toggleTheme(!isDark)} checked={!isDark} onChange={() => {}}></input>
       <label htmlFor="hide-checkbox" className="toggle">
         <span className="toggle-button">
         </span>
